0f13761c41ebfc572899a3426fcf543ca7781c51
DimaG <dimagilev91@gmail.com>
2024-04-18 02:15:45 +0500

    Alerts

--- Train/Alerts/Alerts.cs
+++ Train/Alerts/Alerts.cs
@@ -11,18 +11,17 @@
             //using var input = new StreamReader(Console.OpenStandardInput());
             //using var output = new StreamWriter(Console.OpenStandardOutput());
 
-            string[] data = input.ReadLine().Split();
-            int n = int.Parse(data[0]);
-            int q = int.Parse(data[1]);
+            int[] data = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            int n = data[0];
+            int q = data[1];
 
             int[] response = new int[n + 1]; 
             int msgNumber = 0;
             for (int i = 0; i < q; i++)
             {
-                string[] query = input.ReadLine().Split();
-
-                int t = int.Parse(query[0]);
-                int id = int.Parse(query[1]);
+                int[] query = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+                int t = query[0];
+                int id = query[1];
 
                 if (t == 1)
                 {
