3eeae841c18466f7c23ac70ffb4e209e74b732d9
DimaG <dimagilev91@gmail.com>
2024-04-17 17:55:15 +0500

    stickers

--- Train/Program.cs
+++ Train/Program.cs
@@ -1,4 +1,5 @@
-﻿using Train.Summator;
+﻿using Train.Stickers;
+using Train.Summator;
 
 namespace Techpoint
 {
@@ -6,10 +7,45 @@ namespace Techpoint
     {
         public static void Main(string[] args)
         {
-            Summator summator= new Summator();
-            summator.Run();
+            Stickers prigramm = new Stickers();
+            string[] files = Directory.GetFiles(@"C:\\Users\\DimaG\Downloads\238");
+
+            foreach (string file in files)
+            {
+                if (file.EndsWith(".Test"))
+                {
+                    File.Delete(file);
+                }
+            }
+
+            foreach (string file in files)
+            {
+                if (!file.EndsWith(".a"))
+                {
+                    prigramm.Run(file);
+                }
+            }
+
+            foreach (string file in files)
+            {
+                if (file.EndsWith(".a"))
+                {
+                    Test(file);
+                }
+            }
             Console.WriteLine("End");
             Console.ReadLine();
+        }
+
+        public static void Test(string inputFilePath)
+        {
+            string testPath = inputFilePath.Replace(".a", ".Test");
+            string orig = File.ReadAllText(inputFilePath).Trim();
+            string myResult = File.ReadAllText(testPath).Trim();
+            if (orig != myResult)
+            {
+                Console.WriteLine($"Test failed:{inputFilePath}");
+            }
         }
     }
 }
---
+++ Train/Stickers/Stickers.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Train.Stickers
+{
+    internal class Stickers
+    {
+        public void Run(string inputFilePath="")
+        {
+            //string outputFilePath = $"{inputFilePath}.Test";
+            //using var input = new StreamReader(inputFilePath);
+            //using var output = new StreamWriter(outputFilePath);
+
+            using var input = new StreamReader(Console.OpenStandardInput());
+            using var output = new StreamWriter(Console.OpenStandardOutput());
+
+            string sticker = input.ReadLine();
+            StringBuilder newSticker = new StringBuilder(sticker);
+
+            int n = int.Parse(input.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = input.ReadLine().Split();
+                int startIndex = int.Parse(data[0]) - 1;
+                int endIndex = int.Parse(data[1]) - 1;
+                string str = data[2];
+
+                newSticker.Remove(startIndex, (endIndex - startIndex) + 1);
+                newSticker.Insert(startIndex, str);
+            }
+
+            output.WriteLine(newSticker);
+        }
+    }
+}
