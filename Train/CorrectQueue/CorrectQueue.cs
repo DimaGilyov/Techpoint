9649db0f9c0c0132e85d8f28c27dac32d3c173f8
DimaG <dimagilev91@gmail.com>
2024-04-23 23:20:37 +0500

    sdfdsfds

--- Train/CorrectQueue/CorrectQueue.cs
+++ Train/CorrectQueue/CorrectQueue.cs
@@ -9,10 +9,6 @@ namespace Train.CorrectQueue
             string outputFilePath = $"{inputFilePath}.Test";
             using var input = new StreamReader(inputFilePath);
             using var output = new StreamWriter(outputFilePath);
-            if (outputFilePath.EndsWith("13.Test"))
-            {
-                int a = 0;
-            }
             //using var input = new StreamReader(Console.OpenStandardInput());
             //using var output = new StreamWriter(Console.OpenStandardOutput());
 
@@ -22,7 +18,10 @@ namespace Train.CorrectQueue
                 int n = int.Parse(input.ReadLine());// количество сообщений в очереди
                 string message = input.ReadLine();
 
-
+                if (outputFilePath.EndsWith("3.Test") && i == 150)
+                {
+                    int a = 0;
+                }
                 List<char> events = message.ToList();
 
                 // 1. Сгенерируем карту событий и интекстов
@@ -42,45 +41,73 @@ namespace Train.CorrectQueue
                     indexes.Add(j);
                 }
 
-                // 2. Удалим все YZ события
                 int xCount = x_indexes.Count;
-                int remMax = (n - xCount) / 2;
-                int remCount = 0;
-                //int yCount = y_indexes.Count;
-                //int zCount = z_indexes.Count;
+                int yCount = y_indexes.Count;
+                int zCount = z_indexes.Count;
+
+                // 2. Удалим все YZ события
+                List<int> yIndexesRemoved = new List<int>();
                 for (int j = 0; j < y_indexes.Count; j++)
                 {
-                    if (remCount == remMax)
+
+                    xCount = x_indexes.Count;
+                    yCount = y_indexes.Count - yIndexesRemoved.Count;
+                    zCount = z_indexes.Count;
+                    if (xCount == yCount + zCount)
                     {
+                        // Прекращаем удалять пары YZ, т.к остались Y и Z что бы собрать пары XY и XZ
                         break;
                     }
                     int y_index = y_indexes[j];
                     int index = z_indexes.FindIndex(e => e > y_index);
                     if (index >= 0)
                     {
-                        y_indexes.RemoveAt(j);
+                        yIndexesRemoved.Add(y_index);
                         z_indexes.RemoveAt(index);
-                        remCount++;
                     }
+                }
+
+                foreach (int index in yIndexesRemoved)
+                {
+                    int j = y_indexes.FindIndex(e => e == index);
+                    y_indexes.RemoveAt(j);
                 }
 
                 // 3. Пробуем собрать все XY и XZ события
                 bool success = true;
-                for (int j = 0; j < x_indexes.Count; j++)
+
+                xCount = x_indexes.Count;
+                yCount = y_indexes.Count;
+                zCount = z_indexes.Count;
+                if (xCount == yCount + zCount)
                 {
-                    int x_index = x_indexes[j];
-                    int y_index = y_indexes.FindIndex(e => e > x_index);
-                    if (y_index >= 0)
+                    for (int j = 0; j < x_indexes.Count; j++)
                     {
-                        x_indexes.RemoveAt(j);
-                        y_indexes.RemoveAt(y_index);
-                    }
-                    else 
-                    {
+                        int x_index = x_indexes[j];
+                        int y_index = y_indexes.FindIndex(e => e > x_index);
                         int z_index = z_indexes.FindIndex(e => e > x_index);
+
+                        int y_val = -1;
+                        int z_val = -1;
+                        if (y_index >= 0)
+                        {
+                            y_val = y_indexes[y_index];
+                        }
                         if (z_index >= 0)
                         {
-                            x_indexes.RemoveAt(j);
+                            z_val = z_indexes[z_index];
+                        }
+
+                        bool yFirst = y_val >= 0 && z_val >= 0 && y_val < z_val || z_val == -1;
+
+                        if (yFirst && y_index >= 0)
+                        {
+                            //x_indexes.RemoveAt(j);
+                            y_indexes.RemoveAt(y_index);
+                        }
+                        else if (z_index >= 0)
+                        {
+                            //x_indexes.RemoveAt(j);
                             z_indexes.RemoveAt(z_index);
                         }
                         else
@@ -89,7 +116,16 @@ namespace Train.CorrectQueue
                             break;
                         }
                     }
+                    if (y_indexes.Count > 0 || z_indexes.Count > 0)
+                    {
+                        success = false;
+                    }
                 }
+                else
+                {
+                    success = false;
+                }
+
 
                 string response = success ? "Yes" : "No";
                 output.WriteLine(response);
